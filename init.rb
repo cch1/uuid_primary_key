19a467e293b7ab7b60f8b041f031cc04ae3e5d32
cch1 <cch1@b5fa333d-9bf8-45b9-b2f2-2709d1971210>
2008-05-16 15:04:53 +0000

    Ensure monkey patch to support UUID PKs in MySQL migrations is not applied when the MySQL ConnectionAdapter is not loaded.
    git-svn-id: svn://cho.hapgoods.com/groupsmarts/uuid_primary_key/trunk@106 b5fa333d-9bf8-45b9-b2f2-2709d1971210

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 require 'GroupSmarts/uuid_primary_key'
-require 'GroupSmarts/uuid_migrations'
+require 'GroupSmarts/uuid_migrations' if defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter)
 #require 'GroupSmarts/uuid_fixtures'
 ActiveRecord::Base.send(:include, GroupSmarts::UUIDPrimaryKey)
